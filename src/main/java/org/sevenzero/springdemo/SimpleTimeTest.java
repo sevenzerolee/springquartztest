674094f0221272c7ddaaef2b32fc08d9d03b0f35
libin <libin2@ctsi.com.cn>
2017-03-30 16:39:57 +0800

    spring quart test

---
+++ src/main/java/org/sevenzero/springdemo/SimpleTimeTest.java
@@ -0,0 +1,26 @@
+package org.sevenzero.springdemo;
+
+import java.util.Date;
+
+/**
+ * 
+ * @author lb
+ * @version 1.0.1
+ * @Description
+ * @date 2017年3月30日
+ * 
+ */
+public class SimpleTimeTest {
+
+	public void doStudy() {
+		System.out.println("It is " + new Date());
+		
+		try {
+			Thread.sleep(8000L);
+		} 
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
