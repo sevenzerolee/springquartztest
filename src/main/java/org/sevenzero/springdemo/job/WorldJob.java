dab5d8bdda0afae80390f9789c23b4737ead8b42
libin <libin2@ctsi.com.cn>
2017-04-20 12:49:47 +0800

    save

--- src/main/java/org/sevenzero/springdemo/job/WorldJob.java
+++ src/main/java/org/sevenzero/springdemo/job/WorldJob.java
@@ -19,6 +19,12 @@ public class WorldJob implements Serializable {
 	private static final long serialVersionUID = 4297964891946540442L;
 
 	public void execute() {
+		try {
+			Thread.sleep(8000L);
+		} 
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		System.out.println("$$$$$$$$$$$$$$$$$$$$ " + new Date());
 	}
 
